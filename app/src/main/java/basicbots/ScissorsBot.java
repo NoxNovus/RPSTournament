5dc0992d0a31ab3488ca06cdb888d857e153da09
Rich <richchen2002@gmail.com>
2023-03-21 11:33:19 -0700

    whoops

--- app/src/main/java/basicbots/ScissorsBot.java
+++ app/src/main/java/basicbots/ScissorsBot.java
@@ -3,7 +3,7 @@ package basicbots;
 import rockpaperscissorstournament.RPSBot;
 import rockpaperscissorstournament.RPSMove;
 
-public class RockBot implements RPSBot {
+public class ScissorsBot implements RPSBot {
 
     public String getName() {
         return "ScissorsBot";
