fb84cd24c0f2eb8308f1a9d5a36ec753e3157f5d
Rich <richchen2002@gmail.com>
2023-03-23 12:13:28 -0700

    doc holliday

--- app/src/main/java/rockpaperscissorstournament/RPSOutcome.java
+++ app/src/main/java/rockpaperscissorstournament/RPSOutcome.java
@@ -3,6 +3,14 @@ package rockpaperscissorstournament;
 public enum RPSOutcome {
     LOSE, DRAW, WIN;
 
+    /**
+     * Computes the opposite of this outcome. Draws are by
+     * definition their own opposite, while wins and losses
+     * are opposites.
+     * 
+     * @return an RPSOutcome corresponding to the opposite outcome
+     * of this one.
+     */
     public RPSOutcome getOpposite() {
         switch (this) {
             case WIN:
@@ -15,6 +23,16 @@ public enum RPSOutcome {
         return null;
     }
 
+    /**
+     * Computes the outcome of a matchup of two RPS moves played by
+     * bot 1 and bot 2 respectively from bot 1's perspective (first argument). 
+     * Flip arguments if the corresponding outcome from bot 2 is desired.
+     * 
+     * @param move1 The move bot 1 played.
+     * @param move2 The move bot 2 played.
+     * @return an RPSOutcome corresponding to the outcome of the play
+     * from bot 1's perspective.
+     */
     public static RPSOutcome getOutcome(RPSMove move1, RPSMove move2) {
         if (move1 == move2)
             return DRAW;
