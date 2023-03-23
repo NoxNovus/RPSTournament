5efb7e8286c7ce0f72a6e4e6ef71069b650e6495
Rich <richchen2002@gmail.com>
2023-03-23 12:31:15 -0700

    BasicLearnerBot fully implemented, first "AI" bot!

--- app/src/main/java/basicbots/BasicLearnerBot.java
+++ app/src/main/java/basicbots/BasicLearnerBot.java
@@ -8,20 +8,44 @@ import java.util.Random;
 
 import rockpaperscissorstournament.RPSBot;
 import rockpaperscissorstournament.RPSMove;
+import rockpaperscissorstournament.RPSOutcome;
 
 public class BasicLearnerBot implements RPSBot {
     private Random rand = new Random();
-    private List<Double> winRates = Arrays.asList(0.0, 0.0, 0.0);
+    
+    // all these lists' values are stored in order rock, paper, scissors
+    private List<Double> winRates = Arrays.asList(0.0, 0.0, 0.0); // this is a fixed-size list
+    private double[] winCounts = {0.0, 0.0, 0.0};
+    private double[] totalCounts = {0.0, 0.0, 0.0};
 
     public String getName() {
         return "BasicLearnerBot";
     }
 
     public RPSMove getMove() {
-        //TODO: update probabilities
-        //currently only playing rock
-
         double maxWinRate = Collections.max(winRates);
+        System.out.println("Current win rates: " + winRates);
         return RPSMove.values()[winRates.indexOf(maxWinRate)];
     }
+
+    @Override
+    public void roundResults(RPSMove myMoveRaw, RPSMove opponentMoveRaw) {
+        // get my move and outcome
+        int myMove = myMoveRaw.ordinal();
+        RPSOutcome outcome = RPSOutcome.getOutcome(myMoveRaw, opponentMoveRaw);
+
+        // count played increments
+        totalCounts[myMove]++;
+
+        // if won, count won increments
+        if(outcome == RPSOutcome.WIN) {
+            winCounts[myMove]++;
+        }
+
+        // recompute values
+        for(int i = 0; i < winRates.size(); i++) {
+            double newWinRate = winCounts[i] / totalCounts[i];
+            winRates.set(i, newWinRate);
+        }
+    } 
 }
